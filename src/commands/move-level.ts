9668b088b7f824c718d7cb289407473434b36e88
gdhpsk <70782191+gdhpsk@users.noreply.github.com>
2025-01-18 22:17:53 -0800

    slight bugfix

--- src/commands/move-level.ts
+++ src/commands/move-level.ts
@@ -23,7 +23,7 @@ const moveLevel: SlashCommand = {
     )
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   execute: async (ctx) => {
-    let req = await fetch(`${siteURI}/levels?position=${(ctx.options.getInteger("position") || 2) - 1}`)
+    let req = await fetch(`${siteURI}/levels?position=${(ctx.options.getInteger("position") || 0)}`)
     let below = await req.json()
     const interaction = await ctx.editReply({
       content: `Moving ${ctx.options.getString(
